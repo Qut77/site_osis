7930d466e2bf623fec3da0fb492524dab7438bbd
Qut77 <kkaliberdaa@gmail.com>
2024-10-03 21:20:14 +0500

    end design, 50 adaptive

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-// Обработчик для ссылок
-document.querySelectorAll('a[href^="#"]').forEach(a => a.addEventListener('click', handleAnchorClick));
-
-// Обработчик для кнопок с onclick, ссылающихся на якорь
-document.querySelectorAll('button[onclick^="window.location.href=\'#"]').forEach(btn => btn.addEventListener('click', handleAnchorClick));
-
-function handleAnchorClick(e) {
+document.querySelectorAll('a[href^="#"]').forEach(a => a.addEventListener('click', e=>{
   e.preventDefault();
 
   document.querySelector('.-menuShown')?.classList.remove('-menuShown');
 
-  let linkid = e.currentTarget.getAttribute("href") || e.currentTarget.getAttribute("onclick").replace("window.location.href='", "").replace("'", "");
+  let linkid = a.getAttribute("href");
   let indent = parseInt(window.getComputedStyle(document.querySelector(linkid))?.['padding-top']) > 70 ? 0 : 40;
   if(document.querySelector('.section-subheader')){indent += 40}
   if(document.querySelector('.section-blogContent .sect-cont')){indent += 80}
@@ -18,4 +12,5 @@ function handleAnchorClick(e) {
     top: document.querySelector(linkid)?.getBoundingClientRect().top - indent,
     behavior: 'smooth'
   });
-}
+}));
+
